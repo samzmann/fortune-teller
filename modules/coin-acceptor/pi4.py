348a0ebc48e4c48ac364920b46ec5406f202e6c0
Samuel Mann <samuelcmann@gmail.com>
2023-07-24 13:26:29 +0200

    improve coin acceptor

--- modules/coin-acceptor/pi4.py
+++ modules/coin-acceptor/pi4.py
@@ -1,16 +1,36 @@
 import RPi.GPIO as GPIO
 from threading import Timer
+from lcd import drivers
+
+GPIO.setmode(GPIO.BCM)
+COIN_PIN = 21 # Physical Pin 40
+GPIO.setup( COIN_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+display = drivers.Lcd()
+display.lcd_display_string(f"Add coins to", 1)
+display.lcd_display_string(f"start", 2)
 
 LAST_IMPULSE_TIMEOUT = 0.150 # 150 ms
 numImpulses = 0
-credit = 0
+totalAmount = 0
 
 timerObj = None
 
 def registerImpulses():
     global numImpulses
-    print(f'registered {numImpulses} impulses')
+    global totalAmount
+
+    if (numImpulses == 1):
+        totalAmount += 1 # 1 Euro coin
+        print("1 Euro")
+    elif (numImpulses == 2):
+        totalAmount += 0.5 # 50 Cents coin
+        print("50 Cents")
+
     numImpulses = 0
+
+    display.lcd_clear()
+    display.lcd_display_string(f"Credit: {totalAmount}", 1)
 
 def onPulseReceived(pin):
     global numImpulses
@@ -24,18 +44,6 @@ def onPulseReceived(pin):
     timerObj = Timer(LAST_IMPULSE_TIMEOUT, registerImpulses)
     timerObj.start()
 
-def sT():
-    global timerObj
-    if timerObj != None and timerObj.is_alive:
-            timerObj.cancel()
 
-    timerObj = Timer(LAST_IMPULSE_TIMEOUT, registerImpulses)
-    timerObj.start()
-
-
-GPIO.setmode(GPIO.BCM)
-counter_pin = 21 # Physical Pin 40
-
-GPIO.setup( counter_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.add_event_detect(counter_pin, GPIO.FALLING)
-GPIO.add_event_callback(counter_pin, onPulseReceived)
+GPIO.add_event_detect(COIN_PIN, GPIO.FALLING)
+GPIO.add_event_callback(COIN_PIN, onPulseReceived)
