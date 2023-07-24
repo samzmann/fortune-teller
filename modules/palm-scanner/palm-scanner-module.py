3dd80fbba587e703949690fd39c5f20df09270f3
Samuel Mann <samuelcmann@gmail.com>
2023-07-24 19:23:14 +0200

    add leds to PalmScanner

--- modules/palm-scanner/palm-scanner-module.py
+++ modules/palm-scanner/palm-scanner-module.py
@@ -2,6 +2,7 @@ import transitions
 from enum import Enum, auto
 
 from ProximitySensor import ProximitySensor
+from ScannerLeds import ScannerLeds
 
 class States(str, Enum):
     IDLE = 'IDLE'
@@ -16,6 +17,7 @@ class Events(str, Enum):
 class PalmReader:
 
     proximitySensor = ProximitySensor()
+    scannerLeds = ScannerLeds()
     
     def __init__(self) -> None:
 
@@ -31,12 +33,26 @@ class PalmReader:
             transitions=stateTransitions,
             initial=States.IDLE
             )
+  
+    def on_exit_IDLE(self):
+        self.scannerLeds.stopAnim()
     
+    def on_exit_DETECTING(self):
+        self.scannerLeds.stopAnim()
+    
+    def on_exit_SCANNING(self):
+        self.scannerLeds.stopAnim()
+    
+    # Will automatically trigger when the machine enters DETECTING state
     def on_enter_DETECTING(self):
+        self.scannerLeds.startBreathAnim()
+
         def onDetect():
             self.trigger(Events.detectHand)            
         self.proximitySensor.startDetect(onDetect)
     
+    def on_enter_SCANNING(self):
+        self.scannerLeds.startScanAnim()
 
 
 palmReader = PalmReader()
