64534a5e8b4082b28d57c60ab60d4c42c07c1f4b
Sam <samuelcmann@gmail.com>
2023-07-26 06:26:05 +0100

    BubbleMotor fix

--- modules/bubble_motor/BubbleMotor.py
+++ modules/bubble_motor/BubbleMotor.py
@@ -1,6 +1,11 @@
 import RPi.GPIO as GPIO
-from ...modules.utils import getMillis
 import random
+
+import time
+
+def getMillis():
+    ms = time.time_ns() // 1_000_000
+    return ms
 
 class BubbleMotor:
     isRunning = False
@@ -11,51 +16,60 @@ class BubbleMotor:
         PIN_IN_A = 16
         PIN_IN_B = 22
         PIN_EN_A = 18
+
         GPIO.setup(16, GPIO.OUT)
         GPIO.setup(22, GPIO.OUT)
         GPIO.setup(PIN_EN_A, GPIO.OUT)
-        pi_pwm = GPIO.PWM(PIN_EN_A,1500)
-        pi_pwm.start(0)
-        pi_pwm.ChangeDutyCycle(30)
+
+        self.pi_pwm = GPIO.PWM(PIN_EN_A,1500)
+        self.pi_pwm.start(0)
 
     def setOn(self):
+        print('setOn')
         GPIO.output(16, True)
         GPIO.output(22, False)
 
     def setOff(self):
+        print('setOff')
         GPIO.output(16, False)
         GPIO.output(22, False)
 
-    def runConstant(self):
-        print('runConstant')
+    def runWithIntervals(self, maxOnMs, maxOffMs, minIntervalMs):
+        self.isRunning = True
+        self.pi_pwm.ChangeDutyCycle(30)
 
         isOn = False
-
-        lastChange = getMillis()
-        nextChange = lastChange
-
-        MAX_ON_MS = 2000
-        MAX_OFF_MS = 2000
-        MIN_INTERVAL_MS = 250
+        nextChange = getMillis()
 
         while self.isRunning:
             now = getMillis()
 
+
             if now > nextChange:
                 if isOn:
                     isOn = False
-                    nextChange = now + random.randint(MIN_INTERVAL_MS, MAX_OFF_MS)
+                    nextChange = now + random.randint(minIntervalMs, maxOffMs)
                     self.setOff()
                 else:
                     isOn = True
-                    nextChange = now + random.randint(MIN_INTERVAL_MS, MAX_ON_MS)
+                    nextChange = now + random.randint(minIntervalMs, maxOnMs)
                     self.setOn()
 
-
-
-
     def runSporadic(self):
-        print('runConstant')
+        MAX_ON_MS = 2000
+        MAX_OFF_MS = 2000
+        MIN_INTERVAL_MS = 250
+        self.runWithIntervals(MAX_ON_MS, MAX_OFF_MS, MIN_INTERVAL_MS)
+
+
+    def runVerySporadic(self):
+        MAX_ON_MS = 1000
+        MAX_OFF_MS = 10000
+        MIN_INTERVAL_MS = 250
+        self.runWithIntervals(MAX_ON_MS, MAX_OFF_MS, MIN_INTERVAL_MS)
     
     def stopRun(self):
         self.isRunning = False
+
+bm = BubbleMotor()
+bm.runVerySporadic()
